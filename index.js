db94d3268c5cdf900cb3992aa8be95de62630ef7
MechaLarry <mecha.larry@gmail.com>
2024-07-05 00:57:09 +0200

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = 'LARRY MECHA';
+
+//welcome message
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+console.log (welcomeMessage);
+
+//excitedwelcomeMessage
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+console.log(excitedWelcomeMessage);
+
+//short greeetings
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+console.log(shortGreeting);
+
+
+
+
